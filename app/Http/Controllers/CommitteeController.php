fa88b235fb122731c70ebab2f695dd138f8c90f5
ngosangns <ngosangns@gmail.com>
2022-12-20 17:31:06 +0700

    feat: resources

---
+++ app/Http/Controllers/CommitteeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCommitteeRequest;
+use App\Http\Requests\UpdateCommitteeRequest;
+use App\Models\Committee;
+
+class CommitteeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCommitteeRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCommitteeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Committee  $committee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Committee $committee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Committee  $committee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Committee $committee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCommitteeRequest  $request
+     * @param  \App\Models\Committee  $committee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCommitteeRequest $request, Committee $committee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Committee  $committee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Committee $committee)
+    {
+        //
+    }
+}
