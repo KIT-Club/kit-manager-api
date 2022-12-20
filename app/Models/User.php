4782ed834738f1ff7a30786489579625da211da2
ngosangns <ngosangns@gmail.com>
2022-12-20 17:46:38 +0700

    fix: user seed

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,29 +11,21 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'user';
+
     protected $fillable = [
+        'username',
         'name',
         'email',
+        'birthday',
+        'avatar',
+        'class',
+        'major',
+        'facebook',
+        'github',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = [];
 }
